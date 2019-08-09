c8d73559273acb60158cc5b9a82954ce1a421194
Rosanpatro <rosanpatro750@gmail.com>
2019-08-09 21:57:07 +0530

    2 files modified

--- components/Login.js
+++ components/Login.js
@@ -1,1 +1,46 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
+
+class Login extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      stt : false,
+      email: "",
+      password: ""
+    }
+  }
+
+  checkLogin = () => {
+    if(this.state.email== "rosan@gmail.com" && this.state.password == "123"){
+      this.setState({
+        stt : true
+      })
+    }
+    else{
+      this.setState({
+        stt : false
+      })
+    }
+  }
+
+  render(){
+    return(
+      <div>
+      {
+        (this.state.stt)?<Redirect to="/home"/>:<Redirect to="/" />
+      }
+      <p>
+      Email: <input onChange={(evt)=> this.setState({email:evt.target.value})} />
+      </p><br/>
+      <p>
+      Password: <input type="password" onChange={(evt)=> this.setState({password:evt.target.value})} />
+      </p><br/>
+      <p>
+      <button onClick={this.checkLogin}>Login</button>
+      </p>
+      </div>
+    )
+  }
+}
+export default Login;
